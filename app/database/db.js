1dea152e76a8d4239f46e649bcc0ff42374e9879
alex8616 <aledar16@gmail.com>
2026-05-02 18:54:58 -0400

    MindepoApp

---
+++ app/database/db.js
@@ -0,0 +1,21 @@
+import * as SQLite from 'expo-sqlite';
+
+const db = SQLite.openDatabaseSync('asistencia.db');
+
+export const initDB = () => {
+  db.execAsync(`
+    CREATE TABLE IF NOT EXISTS asistencia (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      uuid TEXT,
+      email TEXT,
+      fecha TEXT,
+      hora TEXT,
+      tipo TEXT,
+      lat TEXT,
+      lng TEXT,
+      estado_sync TEXT
+    );
+  `);
+};
+
+export default db;
